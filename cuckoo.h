9e0a728f79bd7e54bb2f119817c8d38572cfc053
Daniel Stahl <danstahl1138@gmail.com>
2018-01-20 09:09:12 -0600

    first commit of cuckoo search

---
+++ cuckoo.h
@@ -0,0 +1,193 @@
+#ifndef __CUCKOO__H__
+#define __CUCKOO__H__
+#include "FunctionalUtilities.h"
+#include <cstdlib> 
+#include "SimulateNorm.h"
+
+namespace cuckoo{
+    template<typename T>
+    struct upper_lower{
+        T upper;
+        T lower;
+        upper_lower(T&& lower_, T&& upper_){
+            upper=upper_;
+            lower=lower_;
+        }
+    };
+
+    template<typename T, typename U>
+    auto getRandomParameter(const T& lower, const T& upper, const U& rand){
+        return lower+rand*(upper-lower);
+    }
+
+    template<typename T, typename U>
+    auto getTruncatedParameter(const T& lower, const T& upper, const U& result){
+        return result>upper?upper:(result<lower?lower:result);
+    }
+
+    template<typename T,  typename U>
+    auto getLevy(const T& alpha, const U& rand){
+        return pow(rand, -1.0/alpha);
+    }
+
+    template<typename T, typename U>
+    auto getLevyFlight(const T& currVal, const T& currBest, const T& stepSize, const T& alpha, U&& rand, U&& normRand){
+        return currVal+stepSize*(currVal-currBest)*getLevy(alpha, rand)*normRand;
+    }
+
+    auto getUniform(){
+        return (double)rand()/RAND_MAX;
+    }
+    template<typename T>
+    auto getRandomParameters(const std::vector<upper_lower<T> >& ul){
+        return futilities::for_each(0, (int)ul.size(), [&](const auto& index){
+            return getRandomParameter(ul[index].lower, ul[index].upper, getUniform());
+        });
+    }
+    template<typename Nest, typename ObjFn, typename FnResultArray, typename BestParameter>
+    double getBestNest(Nest* nest, FnResultArray* fnResult, BestParameter* bestParam, const Nest& newNest, const ObjFn& objFn){
+        Nest& nestRef= *nest;
+        FnResultArray& fnResultRef= *fnResult;
+        BestParameter& bestParamRef= *bestParam;
+        for(int i=0; i<nestRef.size(); ++i){
+            auto newFnResult=objFn(newNest[i]);
+            if(newFnResult<=fnResultRef[i]){
+                fnResultRef[i]=newFnResult;
+                nestRef[i]=newNest[i];
+            }
+        }
+        double min=1000000;
+        int indexOfMin=0;
+        for(int i=0; i<nestRef.size(); ++i){
+            if(min>fnResultRef[i]){
+                min=fnResultRef[i];
+                indexOfMin=i;
+            }
+        }
+        bestParamRef=nestRef[indexOfMin];
+        return min;
+    }
+    template<typename Nest, typename ObjFn, typename FnResultArray, typename BestParameter>
+    double getInitBestNest(const Nest& nest, FnResultArray* fnResult, BestParameter* bestParam, const ObjFn& objFn){
+        FnResultArray& fnResultRef= *fnResult;
+        BestParameter& bestParamRef= *bestParam;
+        for(int i=0; i<nest.size(); ++i){
+            auto newFnResult=objFn(nest[i]);
+            if(newFnResult<=fnResultRef[i]){
+                fnResultRef[i]=newFnResult;
+            }
+        }
+        double min=1000000;
+        int indexOfMin=0;
+        for(int i=0; i<nest.size(); ++i){
+            if(min>fnResultRef[i]){
+                min=fnResultRef[i];
+                indexOfMin=i;
+            }
+        }
+        bestParamRef=nest[indexOfMin];
+        return min;
+    }
+
+    template<
+        typename Nest, typename BestParameter, typename T, 
+        typename U, typename Norm, typename Unif
+    >
+    void getCuckoos(
+        Nest* newNest, const Nest& nest, const BestParameter& best, 
+        const std::vector<upper_lower<T > >& ul, 
+        const U& alpha, 
+        Unif&& unif,
+        Norm&& norm
+    ){
+        int n=nest.size();
+        Nest& nestRef= *newNest;
+        for(int i=0; i<n;++i){
+            for(int j=0; j<best.size(); ++j){
+                nestRef[i][j]=getTruncatedParameter(
+                    ul[j].lower, ul[j].upper, 
+                    getLevyFlight(
+                        nest[i][j], best[j], .01, alpha, unif(), norm()
+                    )
+                );
+            }
+        }
+    }
+
+
+    template< typename T>
+    auto getNewNest(const std::vector<upper_lower<T> >& ul, int n){
+        return futilities::for_each(0, n, [&](const auto& index){
+            return getRandomParameters(ul);
+        });
+    }
+
+    template<typename P, typename Index>
+    P getPA(const P& pMin, const P& pMax, const Index& index, const Index& n){
+        return pMax-(pMax-pMin)*index/n;
+    }
+
+    template<typename Nest, typename P, typename T>
+    void emptyNests(Nest* newNest, const Nest& nest, const std::vector<upper_lower<T> >& ul, const P& p){
+        int n=nest.size();
+        Nest& nestRef= *newNest;
+        for(int i=0; i<n; ++i){
+            if(getUniform()>p){
+                nestRef[i]=getRandomParameters(ul);
+            }
+        }
+    }
+
+
+    template<typename ObjFn, typename T>
+    auto optimize(const ObjFn& objFn, const std::vector<upper_lower<T> >& ul, int n, int totalMC, int seed){
+        int numParams=ul.size();
+        srand(seed);
+        auto nest=getNewNest(ul, n);
+        auto newNest=getNewNest(ul, n);
+        double alpha=1.5;
+        double pMin=.05;
+        double pMax=.95;
+        SimulateNorm norm;
+        std::vector<double> fnResult(n, 10000000);
+        std::vector<double> bestParams(numParams, 10000000);
+        std::vector<double> tmpParams(numParams, 10000000);
+
+        double fMin=getInitBestNest(nest, &fnResult, &bestParams, objFn);
+        std::cout<<fnResult[0]<<", "<<fnResult[n-1]<<std::endl;
+        std::cout<<fMin<<std::endl;
+        std::cout<<bestParams[0]<<", "<<bestParams[1]<<std::endl;
+        double fNew;
+        for(int i=0; i<totalMC; ++i){
+            /**Completely overwrites newNest*/
+            getCuckoos(&newNest, nest, bestParams, ul, alpha, 
+                [](){return getUniform();}, 
+                [&](){return norm.getNorm();}
+            );
+            fNew=getBestNest(
+                &nest, 
+                &fnResult, 
+                &tmpParams,//not used until 189...get overwritten below but thats fine
+                newNest,
+                objFn
+            );
+            emptyNests(&newNest, nest, ul, getPA(pMin, pMax, i, totalMC));
+            fNew=getBestNest(
+                &nest, 
+                &fnResult, 
+                &tmpParams,
+                newNest,
+                objFn
+            );
+            if(fNew<fMin){
+                fMin=fNew;
+                bestParams=tmpParams;
+            }
+        }
+        return std::make_tuple(fMin, bestParams);
+    }
+}
+
+
+
+#endif
---
+++ test.cpp
@@ -0,0 +1,85 @@
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+#include "catch.hpp"
+#include <iostream>
+#include "cuckoo.h"
+
+TEST_CASE("Test getBestNest 1", "[Cuckoo]"){
+    std::vector<std::vector<double> > nest(2, std::vector<double>(2, 1.0));
+    std::vector<std::vector<double> > newNest(2, std::vector<double>(2, 4.0));
+    std::vector<double> fitness(2, 2.0);
+    std::vector<double> best(2, 50000);
+    double fmin=cuckoo::getBestNest(&nest, &fitness, &best, newNest, [](const auto& inputs){
+        return inputs[0]*inputs[0]+inputs[1]*inputs[1];
+    });
+    REQUIRE(fitness==std::vector<double>({2.0, 2.0}));
+    REQUIRE(best==std::vector<double>({1.0, 1.0}));
+    REQUIRE(fmin==2.0);
+    REQUIRE(nest==std::vector<std::vector<double> >({
+        std::vector<double>({1.0, 1.0}), 
+        std::vector<double>({1.0, 1.0})
+    }));
+}
+TEST_CASE("Test getBestNest 2", "[Cuckoo]"){
+    std::vector<std::vector<double> > nest(2, std::vector<double>(2, 1.0));
+    std::vector<std::vector<double> > newNest(2, std::vector<double>(2, 4.0));
+    nest[0][0]=3;
+    nest[1][0]=5;
+    nest[0][1]=4;
+    nest[1][1]=2;
+    newNest[0][0]=4;
+    newNest[1][0]=2;
+    newNest[0][1]=5;
+    newNest[1][1]=3;
+    std::vector<double> fitness(2, 2.0);
+    std::vector<double> best(2, 50000);
+    double fmin=cuckoo::getBestNest(&nest, &fitness, &best, newNest, [](const auto& inputs){
+        return inputs[0]*inputs[0]+inputs[1]*inputs[1];
+    });
+    REQUIRE(fitness==std::vector<double>({2.0, 2.0}));
+    REQUIRE(best==std::vector<double>({3.0, 4.0}));
+    REQUIRE(fmin==2.0);
+    REQUIRE(nest==std::vector<std::vector<double> >({
+        std::vector<double>({3.0, 4.0}), 
+        std::vector<double>({5.0, 2.0})
+    }));
+}
+TEST_CASE("Test cuckoos 1 ", "[Cuckoo]"){
+    std::vector<cuckoo::upper_lower<double> > ul;
+    std::vector<std::vector<double> > nest(2, std::vector<double>(2, 1.0));
+    std::vector<std::vector<double> > newNest(2, std::vector<double>(2, 1.0));
+    std::vector<double> best(2, 3.0);
+    cuckoo::upper_lower<double> bounds={-1.0, 1.0};
+    ul.push_back(bounds);
+    ul.push_back(bounds);
+    cuckoo::getCuckoos(&newNest, nest, best, ul, 1.5, [](){return .5;}, [](){return 1.5;});
+    REQUIRE(newNest[0][0]==Approx(.95238));//from matlab ref
+    //std::cout<<nest[0][0]<<", "<<nest[0][1]<<", "<<nest[1][0]<<", "<<nest[1][1]<<std::endl;
+}
+TEST_CASE("Test cuckoos 2", "[Cuckoo]"){
+    std::vector<cuckoo::upper_lower<double> > ul;
+    std::vector<std::vector<double> > nest(2, std::vector<double>(2, 1.0));
+    std::vector<std::vector<double> > newNest(2, std::vector<double>(2, 1.0));
+    std::vector<double> best(2, 3.0);
+    cuckoo::upper_lower<double> bounds={-1.0, 1.0};
+    ul.push_back(bounds);
+    ul.push_back(bounds);
+    cuckoo::getCuckoos(&newNest, nest, best, ul, 1.5, [](){return .5;}, [](){return -1.5;});
+    REQUIRE(newNest[0][0]==1.0);//from matlab ref
+    //std::cout<<nest[0][0]<<", "<<nest[0][1]<<", "<<nest[1][0]<<", "<<nest[1][1]<<std::endl;
+}
+
+TEST_CASE("Test Simple Function", "[Cuckoo]"){
+    std::vector<cuckoo::upper_lower<double> > ul;
+    cuckoo::upper_lower<double> bounds={-4.0, 4.0};
+    ul.push_back(bounds);
+    ul.push_back(bounds);
+    ul.push_back(bounds);
+    ul.push_back(bounds);
+   // ul.push_back(bounds);
+    auto results=cuckoo::optimize([](const std::vector<double>& inputs){
+        return inputs[0]*inputs[0]+inputs[1]*inputs[1]+inputs[2]*inputs[2]+inputs[3]*inputs[3];
+    }, ul, 20, 1000, 42);
+    auto params=std::get<1>(results);
+    std::cout<<params[0]<<", "<<params[1]<<std::endl;
+    REQUIRE(std::get<0>(results)==Approx(0.0));
+}  
